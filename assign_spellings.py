b18591171c2da54a2e62b21d5492131b9cb6c169
Nicolas Micaux <micaux.nicolas@gmail.com>
2025-07-20 21:47:37 +0200

    group into functions, solve bugs, preprocess strings before comparing them

--- assign_spellings.py
+++ assign_spellings.py
@@ -9,6 +9,7 @@ Abbreviations:
 import glob
 import os
 import pickle
+import unicodedata
 from collections import defaultdict
 
 import numpy as np
@@ -20,11 +21,7 @@ from tqdm.contrib.concurrent import process_map
 
 from utils import get_lang_weights
 
-_, LANG_WEIGHTS = get_lang_weights()
-min_weight = min(LANG_WEIGHTS.values())
-LANG_WEIGHTS = defaultdict(lambda: min_weight, LANG_WEIGHTS)
-
-N = 10_000
+N = 7_000
 TO_KEEP = {
     "en",
     "zh_cn",
@@ -51,59 +48,95 @@ TO_KEEP = {
     # "fa",  # TODO: need transliteration
     "zh_cn",
 }
-# 1. Load all pkl files from data/translations/downloads/xx-yy.pkl if yy is "et"
-pkl_dir = "data/translations/downloads"
-pkl_files = glob.glob(os.path.join(pkl_dir, "*.pkl"))
-et_pkls = [
-    f
-    for f in pkl_files
-    if os.path.basename(f)[:2] == "et" and os.path.basename(f)[-6:-4] in TO_KEEP
-]
-# TODO: ALSO USE os.path.basename(f)[:2] == "et"
+
+
+def process_str(s):
+    """
+    Process a string for string comparison.
+    - lowercase it
+    - replace special characters (e.g. accents) with their ASCII equivalents
+    """
+    s = s.lower()
+    s = unicodedata.normalize("NFKD", s)
+    s = "".join(c for c in s if not unicodedata.combining(c))
+    return s
+
 
 # 2. Load all interla tokens
-interla_df = pd.read_csv("output/pronounceable_combinations.csv")
-int_orth_tokens = list(set(interla_df["word"].tolist()))
+def get_int_orth_tokens():
+    interla_df = pd.read_csv("output/pronounceable_combinations.csv")
+    int_orth_tokens = list(set(interla_df["word"].tolist()))
 
-print(len(int_orth_tokens), "interla tokens with et words")
+    print(len(int_orth_tokens), "interla orthographied tokens")
+    return int_orth_tokens
+
+
+int_orth_tokens = get_int_orth_tokens()
 
 # 3. Collect all "et" words and their associated words from all other languages
-# TODO: 30s long
-int_anon_tokens_coocurrences = dict()  # 156: {"fi": [159, 8], "sv": [2, 8]}
-all_y2word = dict()  # To collect all y2word mappings
-all_word2x = dict()  # To collect all word2x mappings
-
-for fpath in tqdm(et_pkls):
-    lang2 = os.path.basename(fpath)[-6:-4]  # e.g. "fi" from "et-fi.pkl"
-
-    with open(fpath, "rb") as f:
-        word2x, y2word, x2ys = pickle.load(f)
-        x2word = {
-            v: k for k, v in word2x.items()
-        }  # Reverse mapping: id -> word in lang1
-        # word2x is a dict: word -> id in lang1
-        # y2word is a dict: id -> word in lang2
-        # x2ys is a dict: id in lang1 -> list of ids in lang2
-
-    all_y2word[lang2] = y2word  # Collect all y2word mappings
-
-    # TODO: WE ASSUME THERE ARE RANKED BY FREQUENCY
-    for x_id, ys in list(x2ys.items())[:N]:  # Limit to N to match interla tokens
-        # reindex x_id
-        word = x2word.get(x_id)
-        if word in all_word2x:
-            new_x_id = all_word2x[word]
-        else:
-            new_x_id = len(all_word2x)
-            all_word2x[word] = new_x_id
-
-        int_anon_tokens_coocurrences.setdefault(new_x_id, dict())
-        # int_anon_tokens_coocurrences[x_id][lang2] = ys[:3]
-        int_anon_tokens_coocurrences[new_x_id][lang2] = ys[0]
-
-print(len(int_anon_tokens_coocurrences), "interla tokens with et words")
 
 
+def step_1():
+    # FIXME: ~30s long
+
+    # 1. Load all pkl files from data/translations/downloads/xx-yy.pkl if yy is "et"
+    pkl_dir = "data/translations/downloads"
+    pkl_files = glob.glob(os.path.join(pkl_dir, "*.pkl"))
+    et_pkls = [
+        f
+        for f in pkl_files
+        if os.path.basename(f)[:2] == "et" and os.path.basename(f)[-6:-4] in TO_KEEP
+    ]
+
+    # TODO: ALSO USE os.path.basename(f)[:2] == "et"
+
+    _, LANG_WEIGHTS = get_lang_weights()
+    min_weight = min(LANG_WEIGHTS.values())
+    LANG_WEIGHTS = defaultdict(lambda: min_weight, LANG_WEIGHTS)
+
+    int_anon_tokens_coocurrences = dict()  # 156: {"fi": [159, 8], "sv": [2, 8]}
+    all_y2normWord = dict()  # To collect all y2word mappings
+    all_y2word = dict()  # To collect all y2word mappings
+    all_word2x = dict()  # To collect all word2x mappings
+
+    for fpath in tqdm(et_pkls):
+        lang2 = os.path.basename(fpath)[-6:-4]  # e.g. "fi" from "et-fi.pkl"
+
+        with open(fpath, "rb") as f:
+            word2x, y2word, x2ys = pickle.load(f)
+            x2word = {
+                v: k for k, v in word2x.items()
+            }  # Reverse mapping: id -> word in lang1
+            # word2x is a dict: word -> id in lang1
+            # y2word is a dict: id -> word in lang2
+            # x2ys is a dict: id in lang1 -> list of ids in lang2
+
+        y2normWord = {k: process_str(v) for k, v in y2word.items()}
+        all_y2normWord[lang2] = y2normWord  # Collect all y2word mappings
+        all_y2word[lang2] = y2word
+
+        # TODO: WE ASSUME THERE ARE RANKED BY FREQUENCY
+        for x_id, ys in list(x2ys.items())[:N]:  # Limit to N to match interla tokens
+            # reindex x_id
+            word = x2word.get(x_id)
+            if word in all_word2x:
+                new_x_id = all_word2x[word]
+            else:
+                new_x_id = len(all_word2x)
+                all_word2x[word] = new_x_id
+
+            int_anon_tokens_coocurrences.setdefault(new_x_id, dict())
+            # int_anon_tokens_coocurrences[x_id][lang2] = ys[:3]
+            int_anon_tokens_coocurrences[new_x_id][lang2] = ys[0]
+
+    print(len(int_anon_tokens_coocurrences), "interla anonymous tokens")
+    return int_anon_tokens_coocurrences, all_y2normWord, all_y2word, LANG_WEIGHTS
+
+
+int_anon_tokens_coocurrences, all_y2normWord, all_y2word, LANG_WEIGHTS = step_1()
+
+
+# FIXME: ~ 20mn long
 def process_int_orth_token(args):
     i, int_orth_token = args
     results = []
@@ -123,10 +156,10 @@ def process_int_orth_token(args):
         #         distances[lang] = avg_dist
 
         for lang, id in assoc_words.items():
-            y2word = all_y2word.get(lang, {})
-            w = y2word.get(id)
-            if w is not None:
-                distance = 1 - ratio(int_orth_token, w, score_cutoff=50) / 100
+            y2normWord = all_y2normWord.get(lang, {})
+            norm_w = y2normWord.get(id)
+            if norm_w is not None:
+                distance = 1 - ratio(int_orth_token, norm_w, score_cutoff=50) / 100
                 distances[lang] = distance
 
         total_weight = sum(LANG_WEIGHTS[lang] for lang in distances)
@@ -141,59 +174,72 @@ def process_int_orth_token(args):
 
 
 # Run in threads and collect all results
-results_pkl_path = "output/all_results.pkl"
-if os.path.exists(results_pkl_path):
-    with open(results_pkl_path, "rb") as f:
-        all_results = pickle.load(f)
-else:
-    all_results = process_map(
-        process_int_orth_token, list(enumerate(int_orth_tokens)), max_workers=12
-    )
-    with open(results_pkl_path, "wb") as f:
-        pickle.dump(all_results, f)
+def step_2():
+    results_pkl_path = "output/coocurrences.pkl"
+    if os.path.exists(results_pkl_path):
+        with open(results_pkl_path, "rb") as f:
+            all_results = pickle.load(f)
+    else:
+        all_results = process_map(
+            process_int_orth_token, list(enumerate(int_orth_tokens)), max_workers=12
+        )
+        with open(results_pkl_path, "wb") as f:
+            pickle.dump(all_results, f)
 
-matching_pkl_path = "output/interla_et_matching.pkl"
-if os.path.exists(matching_pkl_path):
-    with open(matching_pkl_path, "rb") as f:
-        matching = pickle.load(f)
-else:
-    # Build biadjacency matrix
-    nb_orth_tokens = len(int_orth_tokens)
-    nb_anon_tokens = len(int_anon_tokens_coocurrences)
-    A_list = list(range(nb_orth_tokens))
-    B_list = list(int_anon_tokens_coocurrences.keys())
+    # TODO: Flatten the list of results
+    return all_results
 
-    # Use a dense numpy array for insertion
-    biadjacency = np.ones(
-        (len(A_list), len(B_list)), dtype=np.float64
-    )  # default to 1.0
 
-    for result_list in tqdm(all_results):
-        for int_orth_token, int_anon_token, avg_dist in result_list:
-            # assert int_orth_token < nb_orth_tokens, (
-            #     f"int_orth_token {int_orth_token} >= {nb_orth_tokens}"
-            # )
-            # assert int_anon_token < nb_anon_tokens, (
-            #     f"int_anon_token {int_anon_token} >= {nb_anon_tokens}"
-            # )
-            # assert 0 <= avg_dist <= 1.0, f"avg_dist {avg_dist} not in [0, 1]"
-            biadjacency[int_orth_token, int_anon_token] = avg_dist
+all_results = step_2()
 
-    # Compute the minimum weight full matching using linear_sum_assignment
-    row_ind, col_ind = linear_sum_assignment(biadjacency, maximize=False)
 
-    # Build matching dict: int_orth_token -> int_anon_token
-    matching = {i.item(): j.item() for i, j in zip(row_ind, col_ind)}
+def step_3():
+    matching_pkl_path = "output/interla_matching.pkl"
+    if os.path.exists(matching_pkl_path):
+        with open(matching_pkl_path, "rb") as f:
+            matching = pickle.load(f)
+    else:
+        # Build biadjacency matrix
+        biadjacency_dump_path = "output/biadjacency_matrix.npy"
 
-    # Save the matching to a file
-    with open(matching_pkl_path, "wb") as f:
-        pickle.dump(matching, f)
+        if os.path.exists(biadjacency_dump_path):
+            biadjacency = np.load(biadjacency_dump_path)
+        else:
+            nb_orth_tokens = len(int_orth_tokens)
+            nb_anon_tokens = len(int_anon_tokens_coocurrences)
+
+            # Use a dense numpy array for insertion
+            biadjacency = np.ones(
+                (nb_orth_tokens, nb_anon_tokens), dtype=np.float16
+            )  # default to 1.0
+
+            for result_list in tqdm(all_results):
+                for int_orth_token, int_anon_token, avg_dist in result_list:
+                    biadjacency[int_orth_token, int_anon_token] = avg_dist
+
+            # Dump the biadjacency matrix for debugging
+            np.save(biadjacency_dump_path, biadjacency)
+
+        # Compute the minimum weight full matching using linear_sum_assignment
+        row_ind, col_ind = linear_sum_assignment(biadjacency, maximize=False)
+
+        # Build matching dict: int_orth_token -> int_anon_token
+        matching = {i.item(): j.item() for i, j in zip(row_ind, col_ind)}
+
+        # Save the matching to a file
+        with open(matching_pkl_path, "wb") as f:
+            pickle.dump(matching, f)
+
+    return matching
+
+
+matching = step_3()
 
 # Then display the words from A with all their associated "w = y2word.get(y_id)"
 for i, int_orth_token in enumerate(int_orth_tokens):
-    j = matching[i]
-    assoc_words = int_anon_tokens_coocurrences.get(j, {})
-    if len(assoc_words) > 30:
+    if i in matching:  # TODO: why some idx don't exist in matching?
+        j = matching[i]
+        assoc_words = int_anon_tokens_coocurrences.get(j, {})
         print(f"Interla: {int_orth_token}")
         for lang, y_id in assoc_words.items():
             y2word = all_y2word.get(lang, {})
